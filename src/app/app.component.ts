aa7a20aef3d222909f9023f69d0840812832d61c
Vanita Bhoyar <vanita.bhoyar282@gmail.com>
2019-08-07 02:24:42 -0700

    added addition logic by javascript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,21 @@ export class AppComponent {
   isEmptyUsername = false;
   username = "";
   title = 'assignment2';
-
+  value1 = 0;
+  value2 = 0;
+  result = 0;
   onUpdateUsername(event) {
     this.isEmptyUsername = true;
   }
 
   onAddUsername(event) {
     this.username = "";
+  }
+  onAddition(event) {
+    var x = document.getElementById("value1") as HTMLInputElement;
+    var y = document.getElementById("value2") as HTMLInputElement;
+    
+    this.result = +x.value + +y.value;
+    console.log(this.result);
   }
 }
